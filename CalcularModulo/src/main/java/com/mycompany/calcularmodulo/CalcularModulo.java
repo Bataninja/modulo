85a4f5e2beddd22253b463016f45a59a80fc83d8
Jhon <Doe@jhon.com>
2024-08-05 20:48:41 -0500

    Se calcula la operación del módulo.

--- CalcularModulo/src/main/java/com/mycompany/calcularmodulo/CalcularModulo.java
+++ CalcularModulo/src/main/java/com/mycompany/calcularmodulo/CalcularModulo.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.calcularmodulo;
+import java.util.Scanner;
 
-/**
- *
- * @author USUARIO
- */
 public class CalcularModulo {
 
+    public static int CalcularModulo(int n1, int n2){
+        return n1 % n2;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner lector = new Scanner(System.in);
+        int numero1 = lector.nextInt();
+        int numero2 = lector.nextInt();
+        int resultado = CalcularModulo(numero1,numero2);
+        System.out.println("El módulo es: " + resultado);
     }
 }
